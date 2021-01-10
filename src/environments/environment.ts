476ead74f6e954b8f78a14f146f588054e8fab57
Fauzi Halabe <fauzihalabe@gmail.com>
2021-01-09 22:35:00 -0300

    inicial

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,6 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBNVAHZTnl_bl4eTzHaczh9EwSocqJc8RY",
-    authDomain: "cardapio-digital-app.firebaseapp.com",
-    projectId: "cardapio-digital-app",
-    storageBucket: "cardapio-digital-app.appspot.com",
-    messagingSenderId: "783573552867",
-    appId: "1:783573552867:web:f4b590053280ebaac17fd6",
-    measurementId: "G-XQRKLY6YP8"
   }
  };
 
